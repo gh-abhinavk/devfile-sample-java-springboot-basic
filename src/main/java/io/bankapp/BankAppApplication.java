13b9ed0102093f53cc25db76012b7d9680033839
Nita Kumari <developer.abhinavk@gmail.com>
2023-07-14 11:06:27 +0530

    Added log

--- src/main/java/io/bankapp/BankAppApplication.java
+++ src/main/java/io/bankapp/BankAppApplication.java
@@ -56,27 +56,6 @@ public class BankAppApplication {
 
 	}
 
-	@PostMapping("/bot")
-	public String botResponse() {
 
-		// Process the request and generate a response
-		String fulfillmentText = processRequest();
-
-		// Construct the webhook response
-		JsonObject jsonResponse = new JsonObject();
-		jsonResponse.addProperty("fulfillmentText", fulfillmentText);
-
-		// Return the webhook response as JSON
-		return jsonResponse.toString();
-	}
-
-	private static String processRequest() {
-		// Implement your business logic here
-		// You can access parameters from the request using jsonRequest.get("parameterName")
-
-		// Generate a response based on the request
-
-		return "This is the webhook response.";
-	}
 
 }
---
+++ src/main/java/io/bankapp/controller/BotController.java
@@ -0,0 +1,34 @@
+package io.bankapp.controller;
+
+import com.google.gson.JsonObject;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BotController {
+
+    @PostMapping("/bot")
+    public String botResponse(@RequestBody Object object) {
+
+        System.out.println(object);
+        // Process the request and generate a response
+        String fulfillmentText = processRequest();
+
+        // Construct the webhook response
+        JsonObject jsonResponse = new JsonObject();
+        jsonResponse.addProperty("fulfillmentText", fulfillmentText);
+
+        // Return the webhook response as JSON
+        return jsonResponse.toString();
+    }
+
+    private static String processRequest() {
+        // Implement your business logic here
+        // You can access parameters from the request using jsonRequest.get("parameterName")
+
+        // Generate a response based on the request
+
+        return "This is the webhook response.";
+    }
+}
