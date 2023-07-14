fbf61c63cf24e87d3a465e4e4c609af08ff6b1f1
vibha <vibhav3339@gmail.com>
2023-07-14 13:23:46 +0530

    change

--- src/main/java/io/bankapp/controller/BotController.java
+++ src/main/java/io/bankapp/controller/BotController.java
@@ -41,15 +41,25 @@ public class BotController {
     private  String processRequest(WebDto webDto) {
         // Implement your business logic here
         // You can access parameters from the request using jsonRequest.get("parameterName")
+    try {
+
 
         // Generate a response based on the request
-        if("Check Balance".equals(webDto.getIntentName())){
-            Customer customer= customerService.getCustomerInfo(Integer.parseInt(webDto.getAccountNumber()));
-            if(customer.getPan().equals(webDto.getPan())){
-                int balance= accountService.getBalance(Integer.parseInt(webDto.getAccountNumber()));
-                return  "Your balance is "+balance;
+        if ("Check Balance".equals(webDto.getIntentName())) {
+            Customer customer = customerService.getCustomerInfo(Integer.parseInt(webDto.getAccountNumber()));
+            if (customer.getPan().equals(webDto.getPan())) {
+                int balance = accountService.getBalance(Integer.parseInt(webDto.getAccountNumber()));
+                return "Your balance is " + balance;
             }
         }
-        return "This is the webhook response.";
+    }catch (Exception e){
+        System.out.println("error");
+
+        e.printStackTrace();
+        return "Please try again.";
+
+
+    }
+        return "We are processing your request";
     }
 }
